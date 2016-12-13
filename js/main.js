da478f9faf3bf14bfa9c29409e2e659a1722094a
Jimmy Dong <jndong88@gmail.com>
2016-12-13 11:45:55 -0800

    added color flashing

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ var round = 0;
 var startBtn = document.querySelector('button');
 //var element = document.getElementsByClassName("cells");
 
-
 /*----event listeners-----*/
 
 document.getElementById('cell1')
@@ -61,17 +60,84 @@ function compTurn() {
   compPattern.push(random);
   playPattern = [];
   roundCounter();
+  displayColors();
+}
 
-  if (random === 1) {
+
+
+//function highlight() {
+//  if (random === 1) {
+//    document.getElementById('cell1').setAttribute("style", "opacity: 1");
+//  } else if (random === 2) {
+//    document.getElementById('cell2').setAttribute("style", "opacity: 1");
+//  } else if (random === 3) {
+//    document.getElementById('cell3').setAttribute("style", "opacity: 1");
+//  } else if (random === 4) {
+//    document.getElementById('cell4').setAttribute("style", "opacity: 1");
+//  }
+//}
+
+
+function unHighlight() {
+    for(var i = 0; i < compPattern.length; i++) {
+     if (compPattern[i] === 1)  {
+      document.getElementById('cell1').removeAttribute("style", "opacity: 1");
+      } else if (compPattern[i] === 2) {
+          document.getElementById('cell2').removeAttribute("style", "opacity: 1");
+        } else if (compPattern[i] === 3) {
+          document.getElementById('cell3').removeAttribute("style", "opacity: 1");
+        } else if (compPattern[i] === 4) {
+          document.getElementById('cell4').removeAttribute("style", "opacity: 1");
+        }
+      }
+  }
+
+
+
+
+// timer variables
+var tickResolution = 500;
+var ticksPerColor = 2;
+var ticksBetween = 1;
+var timerId;
+ var random = Math.floor(Math.random()*4+1);
+
+// elements
+var display = document.getElementById('cell4');
+
+
+// helper functions
+function displayColors() {
+  var colorIdx = 0;
+  var tickCount = 0;
+  timerId = setInterval(handleTick, tickResolution);
+  function handleTick() {
+    var showTick = colorIdx * (ticksBetween + ticksPerColor);
+    var hideTick = ticksPerColor + colorIdx * (ticksBetween + ticksPerColor);
+    if (tickCount ===  showTick) {
+   if (compPattern[colorIdx] === 1)  {
     document.getElementById('cell1').setAttribute("style", "opacity: 1");
-  } else if (random === 2) {
-    document.getElementById('cell2').setAttribute("style", "opacity: 1");
-  } else if (random === 3) {
-    document.getElementById('cell3').setAttribute("style", "opacity: 1");
-  } else if (random === 4) {
-    document.getElementById('cell4').setAttribute("style", "opacity: 1");
+    } else if (compPattern[colorIdx] === 2) {
+        document.getElementById('cell2').setAttribute("style", "opacity: 1");
+      } else if (compPattern[colorIdx] === 3) {
+        document.getElementById('cell3').setAttribute("style", "opacity: 1");
+      } else if (compPattern[colorIdx] === 4) {
+        document.getElementById('cell4').setAttribute("style", "opacity: 1");
+      }
+    };
+    if (tickCount ===  hideTick) {
+      //display.innerHTML = '';
+      unHighlight();
+      colorIdx++;
+      if (colorIdx === compPattern.length) clearInterval(timerId);
+    }
+    tickCount++;
   }
 }
+
+
+
+
 
 //adds number to player's array//
 //function pTurn() {
@@ -94,9 +160,20 @@ function roundCounter() {
 //plays computer's array
 function playbackComputer() {
   for(var i = 0; i < compPattern.length; i++) {
-    console.log(compPattern[i]);
-  }
+   if (compPattern[i] === 1)  {
+    document.getElementById('cell1').setAttribute("style", "opacity: 1");
+    } else if (compPattern[i] === 2) {
+        document.getElementById('cell2').setAttribute("style", "opacity: 1");
+      } else if (compPattern[i] === 3) {
+        document.getElementById('cell3').setAttribute("style", "opacity: 1");
+      } else if (compPattern[i] === 4) {
+        document.getElementById('cell4').setAttribute("style", "opacity: 1");
+      }
+    }
 }
+
+
+
 
 
 //compare arrays
@@ -118,9 +195,9 @@ function over(){
 }
 
 //light up
-function light() {
+//function light() {
 
-}
+
 
 
 
